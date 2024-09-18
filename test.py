8b79d379191fc5af4fd9d90846fc99dd1601a15f
Sys-stack <shivasudharsank2008@gmail.com>
2024-09-18 21:10:17 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
 import streamlit as st
-st.title("yo IP webstie")
+if st.checkbox("Show"):
+  st.title("DrAni List")
+  st.header("Introduction")
+  st.subheader("Why DrAni List?")
+st.test('A free website/tool to track your Animes, Series, Manda and Dramas in one website!')
+st.markdown('test')
+from PIL import Image as im
+img = im.open("https://shiv.tixte.co/wallpaperflare.com_wallpaper_(2).jpg")
+st.img(img, width = 200)
+
