4e2d102e204bcd3c1da21df24bd473ef1b913b6f
Sys-stack <shivasudharsank2008@gmail.com>
2024-09-18 21:11:37 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ if st.checkbox("Show"):
   st.title("DrAni List")
   st.header("Introduction")
   st.subheader("Why DrAni List?")
-st.test('A free website/tool to track your Animes, Series, Manda and Dramas in one website!')
+st.text('A free website/tool to track your Animes, Series, Manda and Dramas in one website!')
 st.markdown('test')
 from PIL import Image as im
 img = im.open("https://shiv.tixte.co/wallpaperflare.com_wallpaper_(2).jpg")
