aaad9729660abde7a4ad2e24294953f1b33363e8
Sys-stack <shivasudharsank2008@gmail.com>
2024-09-19 19:22:00 +0530

    Update test.py

--- test.py
+++ test.py
@@ -14,10 +14,6 @@ if st.checkbox("Show"):
     st.subheader("Why DrAni List?")
     st.text('A free website/tool to track your Animes, Series, Manga and Dramas in one website!')
     st.markdown('test')
-import streamlit as st
-
-# Set the page title
-st.title("My Streamlit App")
 
 # Inject CSS to set the background image
 background_image_url = "https://shiv.tixte.co/wallpaperflare.com_wallpaper_(2).jpg"
