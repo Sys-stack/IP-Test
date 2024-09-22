9733df7bf1059ec7d6b2fdf7c97681fe79252124
Sys-stack <shivasudharsank2008@gmail.com>
2024-09-22 13:52:21 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,3 @@
+import streamlit as st
 bgimg = 'https://raw.githubusercontent.com/Sys-stack/IP-Test/test/japan-background-digital-art.jpg'
 st.markdown("<h2 style='text-align: center;background-imge: url({'bgimg'});'>DRANILIST</h2>", unsafe_allow_html=True)
